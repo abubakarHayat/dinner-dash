0afc954d852a0454a23a04d93b4f9952b51479bf
abubakar-hayat <abubakar.hayat@devsinc.com>
2022-06-01 21:53:37 +0500

    Initial Commit

---
+++ db/migrate/20220601163734_create_items.rb
@@ -0,0 +1,11 @@
+class CreateItems < ActiveRecord::Migration[5.2]
+  def change
+    create_table :items do |t|
+      t.string :item_title
+      t.string :item_description
+      t.integer :item_price
+
+      t.timestamps
+    end
+  end
+end
