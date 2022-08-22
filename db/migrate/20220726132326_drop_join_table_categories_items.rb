7948ae022a0006de36545217a3dbd166b23b8faf
abubakar-hayat <abubakar.hayat@devsinc.com>
2022-08-22 20:15:23 +0500

    Removed unused migrations

--- db/migrate/20220726132326_drop_join_table_categories_items.rb
+++
@@ -1,5 +0,0 @@
-class DropJoinTableCategoriesItems < ActiveRecord::Migration[5.2]
-  def change
-    drop_table :categories_items
-  end
-end
--- db/migrate/20220726132734_drop_join_table_carts_items.rb
+++
@@ -1,5 +0,0 @@
-class DropJoinTableCartsItems < ActiveRecord::Migration[5.2]
-  def change
-    drop_table :carts_items
-  end
-end
